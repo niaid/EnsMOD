761b30f51594612bcf0e56dd0c248c14b4830979
Jian Song <jian.song@nih.gov>
2023-01-10 20:19:32 -0500

    Edit app.R and README.md files

--- app/app.R
+++ app/app.R
@@ -401,7 +401,7 @@ server <- function(session, input, output){
     
     ## Run Rmarkdown
     inFile2 <- input$file1
-    rmarkdown::render("~/EnsMOD/Ensemble_Methods_for_Outlier_Detection_v2_0_stand_aline_script.Rmd", 
+    rmarkdown::render("~/EnsMOD/Ensemble_Methods_for_Outlier_Detection_v2_0_stand_alone_script.Rmd", 
                       output_format = 'html_document', 
                       output_dir = outputDir, 
                       params=list(
