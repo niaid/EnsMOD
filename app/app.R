d96cfff56cf4a02ec59b7bea95f24faee28fac42
Jian Song <jian.song@nih.gov>
2023-05-16 11:49:23 -0400

    Added more available selections to cutoffs

--- app/app.R
+++ app/app.R
@@ -76,22 +76,22 @@ ui <- fluidPage(
       # Parameters to be selected
       selectInput(inputId = "CCC_min",
                   label = "Cophenetic Correlation Coefficient (CCC):",
-                  choices = c(0.8, 0.9),
+                  choices = c(0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95),
                   selected = 0.8
       ),
       selectInput(inputId = "SC_max",
                   label = "Silhouette Coefficient (SC):",
-                  choices = c(0.25, 0.5),
+                  choices = c(0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5),
                   selected = 0.25
       ),
       selectInput(inputId = "robpca_prob",
                   label = "Robust Sparse PCA algorithm (robpca) cutoff:",
-                  choices = c(0.975, 0.95),
+                  choices = c(0.9, 0.925, 0.95, 0.96, 0.97, 0.975, 0.98, 0.99, 0.995, 0.999, 0.9999, 0.99999, 0.999999),
                   selected = 0.975
       ),
       selectInput(inputId = "PcaGrid_prob",
                   label = "Robust PCA algorithm (PcaGrid) cutoff:",
-                  choices = c(0.975, 0.95),
+                  choices = c(0.9, 0.925, 0.95, 0.96, 0.97, 0.975, 0.98, 0.99, 0.995, 0.999, 0.9999, 0.99999, 0.999999),
                   selected = 0.975
       ),
       fileInput(inputId= "file1",
